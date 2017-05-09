1d7326f8ee71c614107a0406e6ed548855b52c48
ClaudiaSanchez <claudianallely999@hotmail.com>
2017-05-09 12:54:51 -0500

    Experimentos_Python.py added

---
+++ Experimentos_Python.py
@@ -0,0 +1,56 @@
+import pandas
+import numpy
+import os
+import subprocess
+
+def BER(y, yh):
+    u = numpy.unique(y)
+    b = 0
+    for cl in u:
+        m = y == cl
+        b += (~(y[m] == yh[m])).sum() / float(m.sum())
+    return (b / float(u.shape[0])) * 100.
+
+datasets = ['banana','titanic','thyroid','diabetis','breast-cancer','flare-solar','heart','ringnorm','twonorm','german','waveform','splice','image']
+datasets_size = [100,100,100,100,100,100,100,100,100,100,100,20,20]
+datasets_size = [1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+os.chdir('/shared/cnsanchez/EvoDAG')
+#os.chdir('/home/claudia/Documentos/DOCTORADO/CODIGO/EvoDAG')
+folderRes = '../res/TB/'
+folderData = '../data/'
+
+columns=['dataset','error','fitness','size']
+index=numpy.arange(len(datasets))
+df = pandas.DataFrame(columns=columns,index=index)
+for i in range(len(datasets)):
+    archivo = datasets[i]
+    print('------------------------------------------------',archivo)
+    error = 0.0
+    fitness = 0.0
+    size = 0.0
+    for j in range(1,datasets_size[i]+1):
+        fileData = folderData+archivo+'_train_'+str(j)+'.csv'
+        fileParams = folderRes+archivo+'_test_data_'+str(j)+ '.params'
+        fileModel = folderRes+archivo+'_test_data_'+str(j)+'.model'
+        filePredict = folderRes+archivo+'_test_data_'+str(j)+ '.predict'
+        os.system('EvoDAG-params -C -P ' +fileParams+' -u 32 '+fileData)
+        os.system('EvoDAG-train -P '+fileParams+' -m '+fileModel+' -u 32 '+fileData)
+        os.system('EvoDAG-predict -m '+fileModel+' -o '+filePredict+' -u 32 '+fileData)
+        p = subprocess.Popen(['EvoDAG-utils --size ' +fileModel],stdout=subprocess.PIPE,shell=True)
+        size += float(p.stdout.read().decode("utf-8")[6:-1])
+        p = subprocess.Popen(['EvoDAG-utils --fitness ' +fileModel],stdout=subprocess.PIPE,shell=True)
+        fitness += float(p.stdout.read().decode("utf-8")[16:-1])
+        D1 = pandas.read_csv(filePredict,sep=",")
+        D2 = pandas.read_csv(folderData+archivo+'_test_labels_'+str(j)+'.csv',sep=",")
+        error += BER(D1.values,D2.values)
+    error /= datasets_size[i]
+    size /= datasets_size[i]
+    fitness /= datasets_size[i]
+    df['dataset'][i] = archivo
+    df['error'][i] = error
+    df['fitness'][i] = fitness
+    df['size'][i] = size
+    print(df)
+    df.to_csv(folderRes+'ares'+str(datasets_size[i])+'.csv',sep=',')
+print(df)
